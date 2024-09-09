5db86a7eaabb659eb1c1ad5561d0e7785638e994
Xenia Abramova <k.nikolaevna98@gmail.com>
2024-09-09 18:54:23 +0300

    Правки после удаления ветки master

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-// Сборка по умолчанию
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+// // Сборка по умолчанию
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-// })
+// });
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
