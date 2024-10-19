2486a8c14d35d8ff5e97b975e86923bc77b94515
Xenia <k.nikolaevna98@gmail.com>
2024-10-19 23:42:31 +0300

    fix (ProjectTile) анимация на desktop

--- src/components/ProjectsTile/ProjectsTile.jsx
+++ src/components/ProjectsTile/ProjectsTile.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './ProjectsTile.module.scss';
 
 import picture_1 from '../../assets/images/project-tile-1.jpg';
@@ -11,65 +11,91 @@ const items = [
     id: 1,
     picture: picture_1,
     src: '#',
-    title: 'WEB приложения / Сайты',
+    title: 'WEB приложения / <br>Сайты',
   },
   {
     id: 2,
     picture: picture_2,
     src: '#',
-    title: 'IOS / ANDROID / PC',
+    title: 'IOS / ANDROID / <br>PC',
   },
   {
     id: 3,
     picture: picture_3,
     src: '#',
-    title: 'VR / AR / 360° ПАНОРАМЫ',
+    title: 'VR / AR / <br>360° ПАНОРАМЫ',
   },
   {
     id: 4,
     picture: picture_4,
     src: '#',
-    title: 'ВИЗУАЛИЗАЦИИ / АНИМАЦИИ',
+    title: 'ВИЗУАЛИЗАЦИИ / <br>АНИМАЦИИ',
   },
 ];
 
 function ProjectsTile() {
   const animationRef = useRef(null);
 
-  // Функция для сброса анимации
   const resetAnimation = () => {
     if (animationRef.current) {
-      // Удаляем анимацию
-      animationRef.current.classList.remove(styles.animate);
-      setTimeout(() => {
-        // Запускаем анимацию заново
-        animationRef.current.classList.add(styles.animate);
-      }, 50);
+      if (window.innerWidth <= 1439) {
+        animationRef.current.classList.remove(styles.animate);
+        setTimeout(() => {
+          animationRef.current.classList.add(styles.animate);
+        }, 10);
+      } else {
+        animationRef.current.classList.remove(styles.animate);
+      }
     }
   };
 
   useEffect(() => {
-    const timer = setInterval(resetAnimation, 14100);
-    return () => clearInterval(timer);
+    resetAnimation();
+    const handleResize = () => {
+      resetAnimation();
+    };
+
+    window.addEventListener('resize', handleResize);
+    let timer;
+    if (window.innerWidth <= 1439) {
+      timer = setInterval(resetAnimation, 14200);
+    }
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      if (timer) {
+        clearInterval(timer);
+      }
+    };
   }, []);
+
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   return (
     <section className={styles.ProjectsTile}>
-      {/* <div className={`${styles.ProjectsTile__items} `}> */}
-      <div className={`${styles.ProjectsTile__items} ${styles.animate}`} ref={animationRef}>
-        {items.map(({ id, picture, src, title }) => (
-          <div key={id} className={styles.ProjectsTile__item}>
-            <a className={styles.ProjectsTile__link} href={src}>
-              <div className={styles.ProjectsTile__picture}>
+      <div className={styles.ProjectsTile__container}>
+        <div className={`${styles.ProjectsTile__items} ${styles.animate}`} ref={animationRef}>
+          {items.map(({ id, picture, src, title }, index) => (
+            <div key={id} className={styles.ProjectsTile__item} onMouseEnter={() => setHoveredIndex(index)} onMouseLeave={() => setHoveredIndex(null)}>
+              <div className={`${styles.ProjectsTile__picture} ${hoveredIndex !== null && hoveredIndex !== index ? styles.hide : ''}`}>
                 <img src={picture} alt={title} />
               </div>
-              <div className={styles.ProjectsTile__text}>
-                <div className={styles.ProjectsTile__num}>{String(id).padStart(2, '0')}</div>
-                <div className={styles.ProjectsTile__title}>{title}</div>
-              </div>
-            </a>
-          </div>
-        ))}
+              <a className={styles.ProjectsTile__link} href={src}>
+                <div className={styles.ProjectsTile__text}>
+                  <div className={styles.ProjectsTile__num}>{String(id).padStart(2, '0')}</div>
+                  <div className={styles.ProjectsTile__title} dangerouslySetInnerHTML={{ __html: title }}></div>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+        <div className={styles.ProjectsTile__images}>
+          {items.map(({ id, picture }, index) => (
+            <div key={id} className={`${styles.ProjectsTile__image} ${hoveredIndex === index ? styles.show : ''}`}>
+              <img src={picture} alt={`Image ${id}`} />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
